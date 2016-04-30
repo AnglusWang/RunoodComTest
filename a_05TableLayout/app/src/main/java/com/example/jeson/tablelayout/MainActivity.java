564b7d2d1d7634dcf9dc125e7201ebb433930274
Jeson <1655087079@qq.com>
2016-04-30 13:36:35 +0800

    login登录界面小练

--- a_05TableLayout/app/src/main/java/com/example/jeson/tablelayout/MainActivity.java
+++ a_05TableLayout/app/src/main/java/com/example/jeson/tablelayout/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.shrink_layout);
+        setContentView(R.layout.login);
     }
 }
