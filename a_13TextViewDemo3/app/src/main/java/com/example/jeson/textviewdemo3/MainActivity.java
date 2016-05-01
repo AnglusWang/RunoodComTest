96345b4e1a2890077b24efdd34f56f145262cdd6
Jeson <1655087079@qq.com>
2016-05-01 16:26:39 +0800

    实现字体滚动效果

--- a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
+++ a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
@@ -1,25 +1,29 @@
 package com.example.jeson.textviewdemo3;
 
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView pt_tv;
+//    private TextView pt_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.demo);
 
-        pt_tv = (TextView) findViewById(R.id.pt_tv);
-        Drawable[] drawable = pt_tv.getCompoundDrawables();
-
-        // 数组下表0~3,依次是:左上右下
-        drawable[1].setBounds(100, 0, 200, 200);
-        pt_tv.setCompoundDrawables(drawable[0], drawable[1], drawable[2],
-                drawable[3]);
+//        TextView tv = (TextView) findViewById(R.id.web_tv);
+//        String str = "<font color='blue'><b>百度一下，你就知道~：</b></font><br>";
+//        str += "<a href = 'http://www.baidu.com'>百度</a>";
+//        tv.setText(Html.fromHtml(str));
+//        tv.setMovementMethod(LinkMovementMethod.getInstance());
+//
+//        pt_tv = (TextView) findViewById(R.id.pt_tv);
+//        Drawable[] drawable = pt_tv.getCompoundDrawables();
+//
+//        // 数组下表0~3,依次是:左上右下
+//        drawable[1].setBounds(100, 0, 200, 200);
+//        pt_tv.setCompoundDrawables(drawable[0], drawable[1], drawable[2],
+//                drawable[3]);
     }
 }
