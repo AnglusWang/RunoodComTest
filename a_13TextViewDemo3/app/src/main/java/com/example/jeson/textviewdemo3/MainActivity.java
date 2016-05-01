22e77f014420ea4b2316980b1e548867b6bd3894
Jeson <1655087079@qq.com>
2016-05-01 15:44:45 +0800

    在java代码中修改drawable的位置大小

--- a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
+++ a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.jeson.textviewdemo3;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView pt_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pt_tv = (TextView) findViewById(R.id.pt_tv);
+        Drawable[] drawable = pt_tv.getCompoundDrawables();
+
+        // 数组下表0~3,依次是:左上右下
+        drawable[1].setBounds(100, 0, 200, 200);
+        pt_tv.setCompoundDrawables(drawable[0], drawable[1], drawable[2],
+                drawable[3]);
     }
 }
