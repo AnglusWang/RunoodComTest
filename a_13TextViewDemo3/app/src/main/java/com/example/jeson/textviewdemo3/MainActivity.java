ab7f287b3bbe7c85fda6346c333dca332ba3565e
Anglus Wang <1655087079@qq.com>
2016-05-01 16:23:40 +0800

    Update MainActivity.java

--- a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
+++ a_13TextViewDemo3/app/src/main/java/com/example/jeson/textviewdemo3/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private TextView pt_tv;
 
