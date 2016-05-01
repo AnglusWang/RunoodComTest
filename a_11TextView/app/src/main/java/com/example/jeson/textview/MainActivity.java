45ac66055c6f7b7054ff23fdd1ed48081eb30f5b
Jeson <1655087079@qq.com>
2016-05-01 15:09:55 +0800

    带边框的TextView

--- a_11TextView/app/src/main/java/com/example/jeson/textview/MainActivity.java
+++ a_11TextView/app/src/main/java/com/example/jeson/textview/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.shadow_layout);
+        setContentView(R.layout.demo_one);
     }
 }
