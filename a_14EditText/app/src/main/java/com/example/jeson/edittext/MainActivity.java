3c1c696f5addf5469d042cd1e61a01deff0d32ec
Jeson <1655087079@qq.com>
2016-05-02 10:38:01 +0800

    获取焦点后未能弹出小键盘

---
+++ a_14EditText/app/src/main/java/com/example/jeson/edittext/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jeson.edittext;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+}
