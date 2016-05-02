5890b4d642a7d777743af0c6a97e9b8d0cb15b7e
Jeson <1655087079@qq.com>
2016-05-02 15:57:57 +0800

    带删除按钮的EditText简单实现

---
+++ a_15EditTextDemo1/app/src/main/java/com/example/jeson/edittextdemo1/ClearableEditText.java
@@ -0,0 +1,89 @@
+package com.example.jeson.edittextdemo1;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Jeson on 2016/5/2.
+ * 自定义带删除按钮的EditText
+ */
+public class ClearableEditText extends AppCompatEditText {
+
+    private static final int DRAWABLE_LEFT = 0;
+    private static final int DRAWABLE_TOP = 1;
+    private static final int DRAWABLE_RIGHT = 2;
+    private static final int DRAWABLE_BOTTOM = 3;
+    // 删除按钮drawable
+    private Drawable mClearDrawable;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mClearDrawable = getResources().getDrawable(R.drawable.ic_edit_text_clear);
+        assert mClearDrawable != null;
+        // 设置为drawableRight前需先setBounds
+        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+    }
+
+    /**
+     * 输入内容改变时显示/隐藏删除按钮
+     * 仅当输入内容不为空且获得焦点时才显示删除按钮
+     */
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        setClearIconVisible(hasFocus() && text.length() > 0);
+    }
+
+    /**
+     * 焦点改变时显示/隐藏删除按钮
+     * 仅当输入内容不为空且获得焦点时才显示删除按钮
+     */
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        setClearIconVisible(focused && length() > 0);
+    }
+
+    /**
+     * 点击删除按钮清空输入框
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                Drawable drawable = getCompoundDrawables()[DRAWABLE_RIGHT];
+                // 判断删除按钮是否被点击
+                if (drawable != null && event.getRawX() >= (getRight() - drawable.getBounds().width())) {
+                    setText("");
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 显示/隐藏删除按钮
+     */
+    private void setClearIconVisible(boolean visible) {
+        setCompoundDrawables(getCompoundDrawables()[DRAWABLE_LEFT], getCompoundDrawables()[DRAWABLE_TOP],
+                visible ? mClearDrawable : null, getCompoundDrawables()[DRAWABLE_BOTTOM]);
+    }
+}
