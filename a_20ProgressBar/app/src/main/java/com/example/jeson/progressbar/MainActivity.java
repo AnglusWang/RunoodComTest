4af0da6124091accfc0dcb9ba07a18eebdfa5364
Jeson <1655087079@qq.com>
2016-05-07 19:57:14 +0800

    使用动画来替代圆形进度条(未能实现，animation-list失效)

--- a_20ProgressBar/app/src/main/java/com/example/jeson/progressbar/MainActivity.java
+++ a_20ProgressBar/app/src/main/java/com/example/jeson/progressbar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.jeson.progressbar;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView img_pgbar;
+    private AnimationDrawable ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.demo1);
+
+        img_pgbar = (ImageView) findViewById(R.id.img_pgbar);
+        ad = (AnimationDrawable) img_pgbar.getDrawable();
+        img_pgbar.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ad.start();
+            }
+        }, 100);
     }
 }
