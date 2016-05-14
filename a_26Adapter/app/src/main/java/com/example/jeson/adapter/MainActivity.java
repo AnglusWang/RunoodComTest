743290528f1067bac3a2a3985695a9ec7e0a0913
Jeson <1655087079@qq.com>
2016-05-14 09:41:36 +0800

    Adapter 通过资源文件加入数据

--- a_26Adapter/app/src/main/java/com/example/jeson/adapter/MainActivity.java
+++ a_26Adapter/app/src/main/java/com/example/jeson/adapter/MainActivity.java
@@ -12,9 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] menu = {"语文", "数学", "英语", "物理", "化学", "历史", "生物"};
-        ArrayAdapter<String> my_adapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, menu);
+//        String[] menu = {"语文", "数学", "英语", "物理", "化学", "历史", "生物"};
+//        ArrayAdapter<String> my_adapter =
+//                new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, menu);
+//        ListView my_lv = (ListView) findViewById(R.id.my_lv);
+//        my_lv.setAdapter(my_adapter);
+
+        ArrayAdapter<CharSequence> my_adapter =
+                ArrayAdapter.createFromResource(this, R.array.my_menus, android.R.layout.simple_expandable_list_item_1);
+
         ListView my_lv = (ListView) findViewById(R.id.my_lv);
         my_lv.setAdapter(my_adapter);
     }
