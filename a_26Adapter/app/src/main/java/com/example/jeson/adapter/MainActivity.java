63447c3ef1f362b8cd639360edce2f4e1c489af3
Jeson <1655087079@qq.com>
2016-05-14 09:29:18 +0800

    Adapter 通过数组加入数据

---
+++ a_26Adapter/app/src/main/java/com/example/jeson/adapter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jeson.adapter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] menu = {"语文", "数学", "英语", "物理", "化学", "历史", "生物"};
+        ArrayAdapter<String> my_adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, menu);
+        ListView my_lv = (ListView) findViewById(R.id.my_lv);
+        my_lv.setAdapter(my_adapter);
+    }
+}
